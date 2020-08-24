3efc88e889dba9b8c3860dfddc79f77fc600c1cb
Konstantin Biriukov <kostya-leon@mail.ru>
2020-08-24 15:47:56 -0500

    pk is changed
    pk is changed (Mapbox_API)

--- js/faang.js
+++ js/faang.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibG9iZW5pY2hvdSIsImEiOiJjajdrb2czcDQwcHR5MnFycmhuZmo4eWwyIn0.nUf9dWGNVRnMApuhQ44VSw';
+mapboxgl.accessToken = 'pk.eyJ1Ijoia29uc3RhbnRpbmJpcml1a292IiwiYSI6ImNrMWsxYjc1bjBrdjQzZHBiNTlhbjBqdmwifQ.vAlGhe7KTCajh5VvGfMJow';
 
 const map = new mapboxgl.Map({
     container: 'map',
